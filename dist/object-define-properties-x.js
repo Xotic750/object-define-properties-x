7d2cf302386c4060e91184946e2ba52870e5d603
Graham Fairweather <xotic750@gmail.com>
2019-08-27 22:55:46 +0200

    Maintenance

--- dist/object-define-properties-x.js
+++ dist/object-define-properties-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-20T20:05:59.443Z",
+  "date": "2019-08-27T20:55:37.263Z",
   "describe": "",
   "description": "Sham for Object.defineProperties",
   "file": "object-define-properties-x.js",
